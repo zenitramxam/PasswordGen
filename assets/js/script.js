a8732dedf77426aa73c2d24e89a86c7ea3fd20cc
Max <m412tinez@gmail.com>
2021-05-14 00:25:32 -0500

    added Upper, Lower, Numeric, Special Character variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numer = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var spec = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', `~`];
+
 
 // Write password to the #password input
 function writePassword() {
