30b5b0369aa7b73af442dc822b4ca2787685bd86
Max <m412tinez@gmail.com>
2021-05-14 06:28:26 -0500

    Updated variables that were not assigned and created for loop for random generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,113 +5,82 @@ var upCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', '
 var numer = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var spCh = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', `~`];
 
-
 var pLeng;
 var lower;
 var upper;
 var numb;
 var spCo;
-
 var options;
-
-//
-
-
 
 console.log(lowCase);
 console.log(upCase);
 console.log(numer);
 console.log(spCh);
 
-// Write password to the #password input
-/* function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-*/
-  function writePassword() {
-    var pLeng = parseInt(prompt('Choose a password length between 8 - 128 '));
-      if (pLeng < 8 || pLeng > 128 || !pLeng) {
-        alert("Password length is invalid. Please provide a password length between 8 - 128");
-        return;
-      }
-      else {
-        var lower = confirm('Would you like Lowercase letters in your password?');
-        var upper = confirm('Would you like Uppercase letters in your password?');
-        var numb = confirm('Would you like numbers in your password?');
-        var spCo = confirm('Would you like special characters in your password? etc. "!, @, #, $,"');
-      };
-
-      if (lower && upper && numb && spCo) {
-        options = lowCase.concat(upCase, numer, spCh);
-      }
-      else if (lower && upper && numb && !spCo) {
-        options = lowCase.concat(upCase, numer);
-      }
-      else if (lower && upper && !numb && !spCo) {
-        options = lowCase.concat(upCase);
-      }
-      else if (lower && !upper && !numb && !spCo) {
-        options = lowCase;
-      }
-      //
-      else if (lower && upper && !numb && spCo) {
-        options = spCh.concat(upCase, lowCase);
-      }
-      else if (lower && !upper && !numb && spCo) {
-        options = spCh.concat(lowCase);
-      }
-      else if (!lower && !upper && !numb && spCo) {
-        options = spCh;
-      }
-      //
-      else if (lower && !upper && numb && spCo) {
-        options = numer.concat(lowCase, spCh);
-      }
-      else if (!lower && !upper && numb && spCo) {
-        options = numer.concat(spCh);
-      }
-      else if (!lower && !upper && numb && !spCo) {
-        options = numer;
-      }
-      //
-      else if (!lower && upper && numb && spCo) {
-        options = upCase.concat(numer, spCh);
-      }
-      else if (!lower && upper && numb && !spCo) {
-        options = upCase.concat(numer);
-      }
-      else if (!lower && upper && !numb && !spCo) {
-        options = upper;
-      }
-      else if (!lower && upper && numb && !spCo) {
-        options = upCase.concat(numer);
-      }
-      else if (lower && !upper && numb && !spCo) {
-        options = lowCase.concat(numer);
-      };
-    var password = [];
-
-    for (var i = 0; i < pLeng; i++) {
-      var possOptions = options[Math.floor(Math.random() * options.length)];
-      password.push(possOptions);
-    }
-
-      
-
-
-    
+function writePassword() {
+  var pLeng = parseInt(prompt('Choose a password length between 8 - 128 '));
+  if (pLeng < 8 || pLeng > 128 || !pLeng) {
+    alert("Password length is invalid. Please provide a password length between 8 - 128");
+    return;
   }
+  else {
+    var lower = confirm('Would you like Lowercase letters in your password?');
+    var upper = confirm('Would you like Uppercase letters in your password?');
+    var numb = confirm('Would you like numbers in your password?');
+    var spCo = confirm('Would you like special characters in your password? etc. "!, @, #, $,"');
+  };
+  if (!lower && !upper && !numb && !spCo) {
+    options = alert("Please choose from the options given.");
+  }
+  else if (lower && upper && numb && spCo) {
+    options = lowCase.concat(upCase, numer, spCh);
+  }
+  else if (lower && upper && numb && !spCo) {
+    options = lowCase.concat(upCase, numer);
+  }
+  else if (lower && upper && !numb && !spCo) {
+    options = lowCase.concat(upCase);
+  }
+  else if (lower && !upper && !numb && !spCo) {
+    options = lowCase;
+  }
+  else if (lower && upper && !numb && spCo) {
+    options = spCh.concat(upCase, lowCase);
+  }
+  else if (lower && !upper && !numb && spCo) {
+    options = spCh.concat(lowCase);
+  }
+  else if (!lower && !upper && !numb && spCo) {
+    options = spCh;
+  }
+  else if (lower && !upper && numb && spCo) {
+    options = numer.concat(lowCase, spCh);
+  }
+  else if (!lower && !upper && numb && spCo) {
+    options = numer.concat(spCh);
+  }
+  else if (!lower && !upper && numb && !spCo) {
+    options = numer;
+  }
+  else if (!lower && upper && numb && spCo) {
+    options = upCase.concat(numer, spCh);
+  }
+  else if (!lower && upper && numb && !spCo) {
+    options = upCase.concat(numer);
+  }
+  else if (!lower && upper && !numb && !spCo) {
+    options = upCase;
+  }
+  else if (!lower && upper && numb && !spCo) {
+    options = upCase.concat(numer);
+  }
+  else if (lower && !upper && numb && !spCo) {
+    options = lowCase.concat(numer);
+  };
+  var password = [];
 
-
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-generateBtn.addEventListener("click", function () {
-  ps = writePassword();
-  document.getElementById("password").placeholder = ps;
-});
+  for (var i = 0; i < pLeng; i++) {
+    var possOptions = options[Math.floor(Math.random() * options.length)];
+    password.push(possOptions);
+  }
+}
